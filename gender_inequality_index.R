3c2fbf2402d7ceaac3fedd21aa26756835e7970e
shawnzsya <2662426069@qq.comgit config --global user.name>
2023-11-02 05:28:51 +0800

    map commit

--- gender_inequality_index.R
+++ gender_inequality_index.R
@@ -1,8 +1,21 @@
 # Read in global gender inequality data
-gender_data <- read.csv("E:/GIS/wk4/gender_inequality_data.csv")
+library(sf)
+library(here)
+library(raster)
+library(terra)
+library(fs)
+library(tidyverse)
+library(ggplot2)
+library(farver)
+library(dplyr)
+library(janitor)
+library(tmap)
+#aaaa
 
+gender_data <- read_csv("F:/UCL/Geographic Information Systems and Science (CASA0005)/WK4/homework/HDR21-22_Composite_indices_complete_time_series.csv",
+                           locale = locale(encoding = "latin1"))
 # Read in spatial data of the world
-world_spatial_data <- sf::st_read("E:/GIS/wk4/World_Countries_Generalized/World_Countries_Generalized.shp")
+world_spatial_data <- st_read("F:/UCL/Geographic Information Systems and Science (CASA0005)/WK4/homework/World_Countries_Generalized/World_Countries_Generalized.shp")
 
 # Calculate the difference in inequality
 diff_data <- gender_data %>%
@@ -14,3 +27,6 @@ diff_data <- gender_data %>%
 merged_data <- dplyr::left_join(world_spatial_data, diff_data, by = c("COUNTRY" = "country"))
 
 #test git pull
+tm_shape(merged_data) + 
+  tm_fill("inequality_difference", style = "quantile", palette = "Blues")
+qtm(merged_data, fill = "inequality_difference", col = "reds")
